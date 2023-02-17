5ace08b0386231bc07984d9c3490c24b5ab19441
Danilo Santos <danilo.aparecido.santos@gmail.com>
2023-02-17 19:24:01 -0300

    consumer dados do websocket no spark

--- producer.py
+++ producer.py
@@ -16,19 +16,16 @@ print (f'Recebendo solicitação de {address}')
 
 # seu producer manda mensagem para o socket aberto
 messages = [
-  'Mensagem A',
-  'Mensagem B',
-  'Mensagem C',
-  'Mensagem D',
-  'Mensagem E',
-  'Mensagem F',
-  'Estou animado com o curso'
+  'Mensagem1 ',
+  'Mensagem2 ',
+  'Mensagem3 ',
+  'ultima mensagem '
 ]
 
 for message in messages:
   message = bytes (message, 'utf-8')
   conn.send(message)
-  time.sleep(4)
+  time.sleep(2)
 
 conn.close()
 
